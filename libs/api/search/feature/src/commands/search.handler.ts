eb8b53b5bc89c4b01aa36f06613f39d01debe082
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-12 12:56:39 +0200

    Updated search feature

---
+++ libs/api/search/feature/src/commands/search.handler.ts
@@ -0,0 +1,14 @@
+import {
+    SearchCommand
+} from '@mp/api/profiles/util';
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
+
+@CommandHandler(SearchCommand)
+export class CreateSearchHandler
+implements ICommandHandler<SearchCommand>
+{
+  constructor(private publisher: EventPublisher) {}
+  async execute(command: SearchCommand) {
+    console.log("Hello there");
+  }
+}
--- libs/api/search/feature/src/search.service.ts
+++ libs/api/search/feature/src/search.service.ts
@@ -1,3 +1,8 @@
+import {
+  ISearchRequest,
+  ISearchResponse,
+  SearchCommand
+} from '@mp/api/search/util';
 import { Injectable } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 
@@ -5,4 +10,12 @@ import { CommandBus } from '@nestjs/cqrs';
 export class SearchService {
   constructor(private commandBus: CommandBus) {}
   //TODO your function here
+  async getSearchRequest(
+    request: ISearchRequest
+  ): Promise<ISearchResponse> {
+    return await this.commandBus.execute<
+      SearchCommand,
+      ISearchResponse
+    >(new SearchCommand(request));
+  }
 }
