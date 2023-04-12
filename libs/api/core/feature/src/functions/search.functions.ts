632f82a680322356f702794ac99adb75c41930e6
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-12 12:37:25 +0200

    Updated Core with search functions

---
+++ libs/api/core/feature/src/functions/search.functions.ts
@@ -0,0 +1,18 @@
+import { SearchService } from '@mp/api/search/feature';
+import {
+    ISearchRequest,
+    ISearchResponse,
+} from '@mp/api/search/util';
+import { NestFactory } from '@nestjs/core';
+import * as functions from 'firebase-functions';
+import { CoreModule } from '../core.module';
+
+export const getSearchRequest = functions.https.onCall(
+    async (
+      request: ISearchRequest
+    ): Promise<ISearchResponse> => {
+      const app = await NestFactory.createApplicationContext(CoreModule);
+      const service = app.get(SearchService);
+      return service.getSearchRequest(request);
+    }
+  );
