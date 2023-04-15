ae054514aace33bb5680908a487d5c1b2e9f249e
GD Radloff <danielrad082@gmail.com>
2023-04-15 15:26:11 +0200

    added neccicary folders

---
+++ libs/app/inbox/data-access/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/app-inbox-data-access';
---
+++ libs/app/inbox/data-access/src/lib/app-inbox-data-access.spec.ts
@@ -0,0 +1,7 @@
+import { appInboxDataAccess } from './app-inbox-data-access';
+
+describe('appInboxDataAccess', () => {
+  it('should work', () => {
+    expect(appInboxDataAccess()).toEqual('app-inbox-data-access');
+  });
+});
