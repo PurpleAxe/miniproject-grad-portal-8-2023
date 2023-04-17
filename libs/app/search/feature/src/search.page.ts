1d80505da651bad0569d7c7ce65b001adb376343
LesibaFrans <105129832+LesibaFrans@users.noreply.github.com>
2023-04-17 23:45:49 +0200

    mock-data

--- libs/app/search/feature/src/search.page.ts
+++ libs/app/search/feature/src/search.page.ts
@@ -5,7 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss','search.page.css'],
 })
-export class SearchPageComponent { 
+export class SearchPageComponent {
+  
+  segment = 'people';
+  
+  chatRooms = [
+    { id: 1, name: 'Nikhil', photo: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png', about: "95% of depressed people are CS students :)" },
+    { id: 2, name: 'Serah', photo: 'https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb.jpg', about: "The aliens are coming for us!!" },
+    { id: 3, name: 'Jess', photo: 'https://cdn.icon-icons.com/icons2/2643/PNG/512/female_woman_person_people_avatar_icon_159366.png', about: "God first always" }
+  ];
+  
+  posts = [
+    { id: 1, name: 'James', photo: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png', postInfo: "95% of depressed people are CS students :)", time: "Just now" },
+    { id: 2, name: 'Mark', photo: 'https://i.pinimg.com/564x/a6/58/32/a65832155622ac173337874f02b218fb.jpg', postInfo: "The aliens are coming for us!!", time: "3 hours ago" },
+    { id: 3, name: 'Sabrina', photo: 'https://cdn.icon-icons.com/icons2/2643/PNG/512/female_woman_person_people_avatar_icon_159366.png', postInfo: "God first always",time: "Yesterday" }
+  ];
+
   constructor(){}
   
   getdisplay(elem: number) {
