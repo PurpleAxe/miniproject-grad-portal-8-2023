8240eea3771fe9a3ff3b01a7bd05f8285a1fd63c
LesibaFrans <105129832+LesibaFrans@users.noreply.github.com>
2023-04-17 23:44:25 +0200

    html-and-display-function

--- libs/app/search/feature/src/search.page.ts
+++ libs/app/search/feature/src/search.page.ts
@@ -5,4 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss','search.page.css'],
 })
-export class SearchPageComponent { }
+export class SearchPageComponent { 
+  constructor(){}
+  
+  getdisplay(elem: number) {
+   let tabs = ["peopleTab","groupsTab","postsTab","eventsTab"];
+   for (var i=0; i<4; i++)
+   {
+     const tab = document.getElementById(tabs[i]);
+     if (tab){
+      tab.style.display = "none"
+     }
+   }
+   const tab = document.getElementById(tabs[elem-1]);
+   if (tabs[elem-1] === "postsTab"){
+    this.segment = 'posts'
+   }else if (tabs[elem-1] === "peopleTab"){
+    this.segment = 'people'
+   }
+   
+   if (tab){
+   tab.style.display = "block";
+   }
+  }
+}
