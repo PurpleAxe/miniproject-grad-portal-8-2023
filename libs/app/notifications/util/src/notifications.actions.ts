7f52f2b7dde11aab3ab333d25f24e86bd8ec3902
ShuaibNuruddin <95973048+ShuaibNuruddin@users.noreply.github.com>
2023-04-20 17:15:38 +0200

    Create notifications.actions.ts

--- libs/app/notifications/util/src/notifications.actions.ts
+++ libs/app/notifications/util/src/notifications.actions.ts
@@ -1,10 +1,22 @@
-//import { INotifications } from '@mp/api/notifications/util';
-
+import { INotifications } from '@mp/api/notifications/util';
+//some things will be changed
 export class AddNotification {
   public readonly type = '[Notifications] AddNotification'
-  //  constructor(public readonly notification: INotification | null) { }
+  constructor(public readonly notification: INotifications | null) { }
 }
 
 export class DeleteNotification {
   static readonly type = '[Notifications] DeleteNotification'
+}
+
+export class CreateNotification {
+  static readonly type = '[Notifications] CreateNotification'
+}
+
+export class MarkAsReadNotification {
+  static readonly type = '[Notifications] MarkAsReadNotification'
+}
+
+export class GetNotifications {
+  static readonly type = '[Notifications] GetNotifications'
 }
