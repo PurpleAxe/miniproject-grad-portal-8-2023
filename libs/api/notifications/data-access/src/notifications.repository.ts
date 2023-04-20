105d37bebd42a72093b5b66b1d9ec1f5c62b2863
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-20 21:38:37 +0200

    Update notifications.repository.ts

--- libs/api/notifications/data-access/src/notifications.repository.ts
+++ libs/api/notifications/data-access/src/notifications.repository.ts
@@ -1,6 +1,7 @@
 import { INotificationBox, INotifications } from '@mp/api/notifications/util';
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import { get } from 'http';
 @Injectable()
 export class NotificationsRepository {
   async createNotifications(notifications: INotificationBox) {
@@ -45,7 +46,30 @@ export class NotificationsRepository {
   }
 
   async markNotificationsAsRead(notificationsBox: INotificationBox) {
-    return;
+    return await admin
+    .firestore()
+    .collection("notifications")
+    .doc(notificationsBox.user.userId)
+    .get().then((doc) => {
+    if (doc.exists) {
+      var oldNotificationsBox = doc.data();
+      for(var oldNotifications of oldNotificationsBox.inbox)
+        for(var notifications of notificationsBox.inbox)
+          if(oldNotifications.notificationID == notifications.notificationID)
+            oldNotifications.read = true
+      
+      admin
+      .firestore()
+      .collection('notifications')
+      .doc(oldNotificationsBox.user.userId)
+      .set(oldNotificationsBox);
+
+    } else {
+        console.log("No such document!");
+    }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
   }
   
 }
