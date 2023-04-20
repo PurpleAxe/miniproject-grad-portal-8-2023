025baae73f392ca1eb2eada322594576a78deae8
ShuaibNuruddin <95973048+ShuaibNuruddin@users.noreply.github.com>
2023-04-21 01:40:49 +0200

    Update notifications.actions.ts

--- libs/app/notifications/util/src/notifications.actions.ts
+++ libs/app/notifications/util/src/notifications.actions.ts
@@ -1,20 +1,35 @@
-import { INotifications } from '@mp/api/notifications/util';
-//some things will be changed
+import { INotifications, INotificationBox } from '@mp/api/notifications/util';
+
+export class Logout {
+  static readonly type = '[Notifications] Logout';
+}
+
+export class SubscribeToNotifications {
+  static readonly type = '[Notifications] SubscribeToNotifications';
+}
+
+export class SetNotifications {
+  static readonly type = '[Notifications] SetNotifications';
+  constructor(public readonly notifications: INotificationBox | null) {}
+}
+
+export class DeleteNotification {
+  static readonly type = '[Notifications] DeleteNotification';
+  constructor(public readonly notification: INotifications | null) {}
+}
+
+export class MarkAsReadNotification {
+  static readonly type = '[Notifications] MarkAsReadNotification';
+  constructor(public readonly notification: INotificationBox | null) {}
+}
+
 export class AddNotification {
   public readonly type = '[Notifications] AddNotification'
   constructor(public readonly notification: INotifications | null) { }
 }
 
-export class DeleteNotification {
-  static readonly type = '[Notifications] DeleteNotification'
-}
-
 export class CreateNotification {
   static readonly type = '[Notifications] CreateNotification'
-}
-
-export class MarkAsReadNotification {
-  static readonly type = '[Notifications] MarkAsReadNotification'
 }
 
 export class GetNotifications {
