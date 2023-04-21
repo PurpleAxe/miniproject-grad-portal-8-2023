d2f2197fa2a118da48cf4cbd53104cd3966b11af
ShuaibNuruddin <95973048+ShuaibNuruddin@users.noreply.github.com>
2023-04-21 21:18:56 +0200

    Update read-notifications.request.ts

--- libs/api/notifications/util/src/requests/read-notifications.request.ts
+++ libs/api/notifications/util/src/requests/read-notifications.request.ts
@@ -1,5 +1,5 @@
 import { INotificationBox } from "../interfaces";
 
 export interface IReadNotificationsRequest {
-    notificationBox: INotificationBox;
+    notificationBox?: INotificationBox|null|undefined;
 }
