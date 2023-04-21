1debe0a0b2fdb08ff623283d228aecb0c87b851a
AmberLz <u18004874@tuks.co.za>
2023-04-21 22:29:57 +0200

    Update notifications.page.ts

--- libs/app/notifications/feature/src/notifications.page.ts
+++ libs/app/notifications/feature/src/notifications.page.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { INotificationBox, INotifications } from '@mp/api/notifications/util';
+import { NotificationsState } from '@mp/app/notifications/data-access';
+import { Observable } from 'rxjs';
+import { Select, Store } from '@ngxs/store';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.page.html',
   styleUrls: ['./notifications.page.scss'],
 })
-export class NotificationsPageComponent { }
+export class NotificationsPageComponent {
+  @Select(NotificationsState.notificationBox) notifications$!: Observable<INotificationBox | null>;
+
+  constructor(
+    private alertController: AlertController,
+    private readonly store: Store
+  ) { }
+
+  ngOnInit() {
+    console.log(this.notifications$);
+  }
+}
+
