6418a9f4b59a584d322380f0625db08285342d26
Megan Hugo <bellvlekkie@gmail.com>
2023-04-22 16:55:42 +0200

    created CreatePost action

--- libs/app/post/util/src/post.actions.ts
+++ libs/app/post/util/src/post.actions.ts
@@ -1,4 +1,11 @@
-export class PostLike{
-    static readonly type = '[PostLike] Post Like';
-    constructor(public payload: { uid: string }) {}
+import { Timestamp } from 'firebase-admin/firestore';
+
+export class CreatePost{
+    static readonly type = '[CreatePost] Create Post';
+    constructor(public payload: { 
+        body: string;
+        department:string;
+        challenge:string;
+        timestamp:Timestamp;
+    }) {}
 } 
