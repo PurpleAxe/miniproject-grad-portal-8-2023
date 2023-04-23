ff79b3c7557e515c0cc8e24b388c3873ac74ae84
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-23 09:23:26 +0200

    Included export for commandHandler and events

--- libs/api/notifications/feature/src/notifications.module.ts
+++ libs/api/notifications/feature/src/notifications.module.ts
@@ -4,14 +4,15 @@ import { CqrsModule } from '@nestjs/cqrs';
 import {
   InboxCreatedEvent,
   NotificationsCreatedEvent,
+  NotificationsReadEvent
 } from '@mp/api/notifications/util';
-import { CreateNotificationHandler } from './commands';
+import { CreateNotificationHandler, ReadNotificationsHandler } from './commands';
 import { CreateInboxHandler } from './commands/createNotificationBox.command';
 import { NotificationsSagas } from './notifications.sagas';
 import { NotificationsService } from './notifications.service';
 
-export const CommandHandlers = [CreateNotificationHandler, CreateInboxHandler];
-export const EventHandlers = [NotificationsCreatedEvent, InboxCreatedEvent];
+export const CommandHandlers = [CreateNotificationHandler, CreateInboxHandler, ReadNotificationsHandler];
+export const EventHandlers = [NotificationsCreatedEvent, InboxCreatedEvent, NotificationsReadEvent];
 
 @Module({
   imports: [CqrsModule, NotificationsDataAccessModule],
