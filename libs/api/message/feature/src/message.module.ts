4e8e7dcf871e6598b8a0026bfbd3b4f80a34f762
GD Radloff <danielrad082@gmail.com>
2023-04-23 17:29:10 +0200

    Potential fix for message module

--- libs/api/message/feature/src/message.module.ts
+++ libs/api/message/feature/src/message.module.ts
@@ -1,12 +1,15 @@
 import { MessageModule as MessageDataAccessModule } from '@mp/api/message/data-access';
+import {UsersModule, UsersRepository} from '@mp/api/users/data-access';
 import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import {
   SendMessageHandler,
   DeleteMessageHandler,
+  CreateConversationHandler,
 } from './commands';
 
 import {
+  ConversationCreatedHandler,
   MessageDeletedHandler,
   MessageSentHandler
 } from './events';
@@ -16,16 +19,18 @@ import {MessageService} from './message.service';
 
 export const CommandHandlers = [
   SendMessageHandler,
-  DeleteMessageHandler
+  DeleteMessageHandler,
+  CreateConversationHandler,
 ]
 
 export const EventHandlers = [
   MessageSentHandler,
   MessageDeletedHandler,
+  ConversationCreatedHandler,
 ]
 
 @Module({
-  imports: [CqrsModule, MessageDataAccessModule],
+  imports: [CqrsModule, MessageDataAccessModule, UsersModule],
   providers : [
     MessageService,
     ...CommandHandlers,
