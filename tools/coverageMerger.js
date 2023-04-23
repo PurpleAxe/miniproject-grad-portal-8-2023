d244ecdeb769e65ecfe715d534aec9af4dca390a
Andreas Visagie u21430901 <u21430901@tuks.co.za>
2023-04-24 01:46:14 +0200

    fix codecov file

--- tools/coverageMerger.js
+++ tools/coverageMerger.js
@@ -4,17 +4,12 @@ const glob = require('glob');
 const fs = require('fs');
 const path = require('path');
 
-const getLcovFiles = function (src) {
-  return new Promise((resolve, reject) => {
-    glob(`${src}/**/lcov.info`, (error, result) => {
-      if (error) return reject(error);
-      resolve(result);
-    });
-  })
+const getLcovFiles = function () {
+ return glob.sync('coverage/**/lcov.info');
 };
 
 (async function(){
-  const files = await getLcovFiles('coverage');
+  const files = getLcovFiles();
   const mergedReport = files.reduce((mergedReport, currFile) => mergedReport += fs.readFileSync(currFile), '');
   await fs.writeFile(path.resolve('./coverage/lcov.info'), mergedReport, (err) => {
     if (err) throw err;
