2bfc990397d890a9a80364cc595cd1ca76409b90
Megan Hugo <bellvlekkie@gmail.com>
2023-04-23 11:34:47 +0200

    Added clicked on like/dislike UI

--- libs/app/feed/ui/src/lib/card/card.component.ts
+++ libs/app/feed/ui/src/lib/card/card.component.ts
@@ -7,11 +7,38 @@ import { Component, Input } from '@angular/core';
 })
 export class CardComponent {
   @Input() content!: any;
+
+  likeNum = 0;
+  dislikeNum = 0;
+  isLiked = false;
+  isDisliked = false;
+
+  commentNum = 0;
+
   Like(){
-    console.log("Like button");
+    if (this.isDisliked){
+      this.isDisliked = !this.isDisliked;
+      this.dislikeNum--;
+    }
+    if (this.likeNum > 0){
+      return;
+    }
+    this.isLiked = !this.isLiked;
+    this.likeNum++;
+    console.log("Like button" + this.likeNum);
+
   }
   Dislike(){
-    console.log("Dislike button");
+    if (this.isLiked){
+      this.isLiked = !this.isLiked;
+      this.likeNum--;
+    }
+    if (this.dislikeNum > 0){
+      return;
+    }
+    this.isDisliked = !this.isDisliked;
+    this.dislikeNum++;
+    console.log("Dislike button" + this.likeNum);
   }
   Comment(){
     console.log("Comment button");
