d51a695c719521b66bc2cf1072d2946347092440
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-24 17:04:18 +0200

    Update feed.interface.ts

--- libs/api/feed/util/src/interfaces/feed.interface.ts
+++ libs/api/feed/util/src/interfaces/feed.interface.ts
@@ -1,7 +1,9 @@
 import { Timestamp } from "firebase-admin/firestore";
 import { IPost } from "@mp/api/post/util";
+import { IProfile } from "@mp/api/profiles/util";
 
 export interface IFeed {
+  user: IProfile,
   posts: IPost[];
 }
 
