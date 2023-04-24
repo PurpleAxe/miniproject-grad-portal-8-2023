a0c265a1a6ea73429faa36302f3e4b809bcd6300
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-24 17:11:12 +0200

    Update feed.model.ts

--- libs/api/feed/feature/src/models/feed.model.ts
+++ libs/api/feed/feature/src/models/feed.model.ts
@@ -1,7 +1,42 @@
 import { AggregateRoot } from '@nestjs/cqrs';
+import { IPost } from '@mp/api/post/util';
+import { IFeed } from '@mp/api/feed/util'; 
+import { GetFeedEvent } from '@mp/api/feed/util';
+import { IProfile } from '@mp/api/profiles/util';
 
 export class Feed extends AggregateRoot implements IFeed {
-  constructor() {
+  constructor(
+    public readonly user: IProfile,
+    public readonly posts: IPost[]
+    ) {
     super();
   }
+
+    fetchHomeFeed() {
+      this.apply(new GetFeedEvent(this.toJSON()));
+    }
+
+    // fetchDiscoryFeed() {
+    //   this.apply(new GetFeedEvent(this.toJSON()));
+    // }
+
+    // fetchOwnFeed() {
+    //   this.apply(new GetFeedEvent(this.toJSON()));
+    // }
+
+    static fromData(feed: IFeed): Feed {
+      const instance = new Feed(
+        feed.user,
+        feed.posts
+      );
+      return instance;
+    }
+
+    toJSON(): IFeed {
+      return {
+        user: this.user,
+        posts: this.posts
+      };
+    }
+  
 }
