479b46189082737544c17bfe52292e70a11bea12
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-25 09:41:02 +0200

    Update feed.repository.ts

--- libs/api/feed/data-access/src/feed.repository.ts
+++ libs/api/feed/data-access/src/feed.repository.ts
@@ -1,9 +1,24 @@
 import { IFeed } from '@mp/api/feed/util';
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
 @Injectable()
 export class FeedRepository {
   async getFeed(feed: IFeed) {
-
+    const f = await admin
+    .firestore()
+    .collection('profiles')
+    .where("userId", "==", feed.user.userId)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          // console.log(doc.data().posts);
+          return doc.data().posts
+      });
+  });
   }
+
+
+
+
 }
