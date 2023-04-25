9fb36e30762f5e7dcaa75920efc97f765e788928
AmberLz <u18004874@tuks.co.za>
2023-04-25 09:31:47 +0200

    Added markAsRead function
    Added click event to call markAsRead function

--- libs/app/notifications/feature/src/notifications.page.ts
+++ libs/app/notifications/feature/src/notifications.page.ts
@@ -4,14 +4,14 @@ import { INotificationBox, INotifications } from '@mp/api/notifications/util';
 import { NotificationsState } from '@mp/app/notifications/data-access';
 import { Observable } from 'rxjs';
 import { Select, Store } from '@ngxs/store';
-import { SubscribeToNotifications } from '@mp/app/notifications/util';
-import {NotificationsModule} from '@mp/app/notifications/data-access'
+import { SubscribeToNotifications, MarkAsReadNotification } from '@mp/app/notifications/util';
+import { NotificationsModule } from '@mp/app/notifications/data-access'
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.page.html',
   styleUrls: ['./notifications.page.scss'],
 })
-export class NotificationsPageComponent implements OnInit{
+export class NotificationsPageComponent implements OnInit {
   @Select(NotificationsState.notificationBox) notifications$!: Observable<INotificationBox | null>;
 
   // segment = 'notifications';
@@ -38,7 +38,7 @@ export class NotificationsPageComponent implements OnInit{
   //   console.log(NotificationsState.notificationBox);
   //   // this.notifications=NotificationsState.notificationBoxInbox();
   //   // this.notificationBox=NotificationsState.notificationBox;
-    
+
   //   // this.notifications=NotificationsState.notificationBox.inbox;
   // }
   ngOnInit() {
@@ -46,6 +46,11 @@ export class NotificationsPageComponent implements OnInit{
     this.store.dispatch(new SubscribeToNotifications());
     console.log("notifications should be printed after this");
     console.log(this.notifications$);
+  }
+
+  markAsRead(notification: INotifications) {
+    //console.log("markAsRead" + notification)
+    //this.store.dispatch(new MarkAsReadNotification(notification.notificationID));
   }
 }
 
