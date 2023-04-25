2031b0dea52f4760b322b29993ae59b0b97d030c
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-25 16:45:16 +0200

    Update feed.repository.ts

--- libs/api/feed/data-access/src/feed.repository.ts
+++ libs/api/feed/data-access/src/feed.repository.ts
@@ -1,10 +1,63 @@
 import { IFeed } from '@mp/api/feed/util';
+import { IPost } from '@mp/api/post/util';
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 
 @Injectable()
 export class FeedRepository {
-  async getFeed(feed: IFeed) {
+  async getHomeFeed(feed: IFeed): Promise<IPost[]> {
+    // get user
+    var user = null;
+    const f = await admin
+    .firestore()
+    .collection('profiles')
+    .where("userId", "==", feed.user.userId)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        user = doc.data()
+      });
+    });
+
+    //get department users
+    var deptUsers = {};
+    for(var deptUser of user.userDepartments) {
+      const f = await admin
+      .firestore()
+      .collection('departments')
+      .where("departmentId", "==", deptUser)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          for(var u of doc.data().users)
+            deptUsers[u] = 0;
+        });
+      });
+    }
+
+    // get posts
+    var posts = [];
+    for(var userInDepartment of Object.keys(deptUsers)) {
+      if(userInDepartment == user.userId) {
+        continue;
+      }
+      const f = await admin
+      .firestore()
+      .collection('profiles')
+      .where("userId", "==", userInDepartment)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          posts = posts.concat(doc.data().posts);
+        });
+      });
+    }
+
+    return posts
+  }
+
+  async getOwnFeed(feed: IFeed): Promise<IPost[]> {
+    var data = null
     const f = await admin
     .firestore()
     .collection('profiles')
@@ -13,9 +66,33 @@ export class FeedRepository {
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
           // console.log(doc.data().posts);
-          return doc.data().posts
+          data = doc.data().posts;
       });
-  });
+    });
+    return data;
+  }
+
+  async getDiscoveryFeed(feed: IFeed): Promise<IPost[]> {
+    var posts = {};
+    const f = await admin
+    .firestore()
+    .collection('profiles')
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data())
+        posts[doc.data().userId] = doc.data().posts;
+      });
+    });
+
+    delete posts[feed.user.userId]
+    
+    console.log(posts)
+    var response = []
+    for(var p of Object.keys(posts))
+      response = response.concat(posts[p])
+      console.log(response)
+    return response;
   }
 
 
