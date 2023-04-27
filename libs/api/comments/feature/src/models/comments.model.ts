7898111c3d3e40ac09f56fb20b734e29a7c9616f
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-27 21:53:46 +0200

    Update comments.model.ts

--- libs/api/comments/feature/src/models/comments.model.ts
+++ libs/api/comments/feature/src/models/comments.model.ts
@@ -1,7 +1,41 @@
 import { AggregateRoot } from '@nestjs/cqrs';
+import {Timestamp} from "firebase-admin/firestore";
+import { IComment, UpdateCommentsEvent } from "@mp/api/comments/util";
 
-export class Comments extends AggregateRoot implements IComments {
-  constructor() {
+export class Comments extends AggregateRoot implements IComment {
+  constructor(
+    public userID: string,
+    public text: string,
+    public commentID: string,
+    public postID :string,
+    public timestamp?: Timestamp
+  ) {
     super();
   }
+
+  static fromData(comment: IComment): Comments {
+    const instance = new Comments(
+      comment.userID,
+      comment.text,
+      comment.commentID,
+      comment.postID,
+      comment.timestamp
+    );
+    return instance;
+  }
+
+  updateComment() {
+    this.apply(new UpdateCommentsEvent(this.toJSON()))
+  }
+
+  toJSON(): IComment {
+    return {
+      userID: this.userID,
+      text: this.text,
+      commentID: this.commentID,
+      postID: this.postID,
+      timestamp: this.timestamp
+    };
+  }
+
 }
