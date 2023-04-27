e9b61da6d38cd256c29883027d9d65b9bf95ac3b
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-27 10:06:22 +0200

    Update search.repository.ts

--- libs/api/search/data-access/src/search.repository.ts
+++ libs/api/search/data-access/src/search.repository.ts
@@ -3,48 +3,56 @@ import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import { ISearchResponse } from '@mp/api/search/util';
 import { IUser } from '@mp/api/users/util';
+import { IProfile } from '@mp/api/profiles/util';
+import { IPost } from '@mp/api/post/util';
 
 @Injectable()
 export class SearchRepository {
   //TODO: add your functions here
-  async getSearchRequest(search: ISearch) {
-    console.log('getSearchRequest this called');
-
-    const SearchResultConverter = {
-      fromFirestore: (snapshot) => {
-        return snapshot.data() as IUser;
-      },
-      toFirestore: (it: IUser) => it,
-    };
-
-    const query = await admin
-      .firestore()
-      .collection('users')
-      .where(search.field, '==', search.keyword)
-      .withConverter<IUser>(SearchResultConverter)
-      .get();
-    // const responseContent = query.docs.map((doc) => doc.data());
-    const responseContent = query.docs.map((doc) => doc.data() as IUser);
-
-    // let res = [{ any: 'No data' }];
-    // await query.get().then((querySnapshot) => {
-    //   const docs = querySnapshot.docs;
-
-    //   for (const doc of docs) {
-    //     console.log(`Document found: ${doc.data()}`);
-    //     console.log(`Document found at path: ${doc.ref.path}`);
-    //   }
-    //   const responseContent = docs.map((doc) => doc.data());
-    //   console.log(responseContent, '***************');
-    //   res = responseContent;
-    // });
-    // console.log('query didnt find anything');
-    const dummy: ISearch = {
-      field: search.field,
-      keyword: search.keyword,
-      searchResults: responseContent,
-    };
-    console.log('!!!', dummy, '!!!');
-    return dummy;
+  async get_document(id: string, collection: string) {
+    var response = null;
+    const f = await admin
+    .firestore()
+    .collection(collection)
+    .doc(id)
+    .get()
+    response = f.data();
+    return response;
   }
+
+  async get_collection(collection) {
+    const snapshot = await admin.firestore().collection(collection).get()
+    return snapshot.docs.map(doc => doc.data());
+  }
+
+  async search_for(key: string, value: any, collection: string): Promise<IProfile[] | IPost[]> {
+    var response = [];
+    const f = await admin
+    .firestore()
+    .collection(collection)
+    .where(key, "==", value)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        response.push(doc.data());
+      });
+    });
+    return response;
+  }
+
+  async get(key: string, value: any, collection: string) {
+    var response = null;
+    const f = await admin
+    .firestore()
+    .collection(collection)
+    .where(key, "==", value)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        response = doc.data();
+      });
+    });
+    return response;
+  }
+
 }
