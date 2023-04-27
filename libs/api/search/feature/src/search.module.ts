a3f440b9626322b3eb1b5dcacc37827d8c91d1fd
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-27 10:07:32 +0200

    Added command handlers

--- libs/api/search/feature/src/search.module.ts
+++ libs/api/search/feature/src/search.module.ts
@@ -1,12 +1,12 @@
 import { SearchModule as SearchDataAccessModule } from '@mp/api/search/data-access';
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { SearchCommandHandler } from './commands';
+import { SearchUsersCommandHandler, SearchPostsCommandHandler } from './commands';
 import { SearchSagas } from './search.sagas';
 import { SearchService } from './search.service';
 import { SearchEventHandler } from './events';
 
-export const CommandHandlers = [SearchCommandHandler];
+export const CommandHandlers = [SearchUsersCommandHandler, SearchPostsCommandHandler];
 export const EventHandlers = [SearchEventHandler];
 
 @Module({
