d9d99bc699b691cc822c714662c509fb7ba44818
Shuaib Nuruddin <95973048+ShuaibNuruddin@users.noreply.github.com>
2023-04-28 01:30:07 +0200

    Add files via upload

--- libs/app/user-profile/feature/src/user-profile.page.ts
+++ libs/app/user-profile/feature/src/user-profile.page.ts
@@ -6,11 +6,13 @@ import { IProfile } from '@mp/api/profiles/util';
 import { AuthState } from '@mp/app/auth/data-access';
 import { ProfileState } from '@mp/app/profile/data-access';
 import { UserProfileState } from '@mp/app/user-profile/data-access';
-import { SharedPageComponent } from '@mp/app/shared/feature'
+import { SharedModule, SharedPageComponent } from '@mp/app/shared/feature'
 // import { SharedModule } from '@mp/app/shared/feature';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import { Logout } from '@mp/app/auth/util';
 import { SubscribeToUserProfile } from '@mp/app/user-profile/util';//'../../util/src/user-profile.actions';
+import { Timestamp } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user-profile',
@@ -28,12 +30,12 @@ export class UserProfilePageComponent implements OnInit{
   displayName : any;
   userName : any;
 
-  constructor(private router: Router,private readonly store: Store,private renderer: Renderer2,) {
+  constructor(private router: Router,private readonly store: Store,private renderer: Renderer2, private alertController: AlertController) {
 
     this.shared = new SharedPageComponent(new AlertController, store);
 
-    // this.shared.calculateTimeDifference();
-    // this.shared.decreaseTime('hours', 'minutes', 'ses');
+    this.shared.calculateTimeDifference();
+    this.shared.decreaseTime('hours', 'minutes', 'ses');
 
     this.profile$.subscribe((profile)=>{
       console.log(profile);
@@ -61,6 +63,57 @@ export class UserProfilePageComponent implements OnInit{
 
       
     });
+   }
+
+  //  logout() {
+  //   // this.popover.dismiss();
+  //   this.store.dispatch(new Logout());
+  // }
+
+  //  async presentAlert() {
+  //   const alert = await this.alertController.create({
+  //   header: 'Time Out!',
+  //   subHeader: 'Your time has run out.',
+  //   message: 'You have run out of time. Your account is no longer in use.',
+  //   buttons: [{
+  //   text: 'LOGOUT',
+  //   handler: () => {
+  //   this.logout();
+  //   }
+  //   }],
+  //   backdropDismiss: false // prevent dismissing by clicking outside of the alert
+  //   });
+
+  //   await alert.present();
+  // }
+
+   getSeconds(profile: IProfile){
+    //SharedModule.se
+    if (profile.timeLeft?.seconds!=undefined){
+      const seconds = (profile.timeLeft?.seconds-Timestamp.now().seconds)%60;
+      // console.log(profile.timeLeft?.seconds-Timestamp.now().seconds);
+      // if (profile.timeLeft?.seconds-Timestamp.now().seconds<=0){
+      //   this.presentAlert();
+      // }
+      return seconds;
+    }
+    return 0;
+   }
+
+   getMinutes(profile: IProfile){
+    if (profile.timeLeft?.seconds!=undefined){
+      const minutes = Math.floor(((profile.timeLeft?.seconds-Timestamp.now().seconds)%3600)/60);
+      return minutes;
+    }
+    return 0;
+   }
+
+   getHours(profile: IProfile){
+    if (profile.timeLeft?.seconds!=undefined){
+      const hours = Math.floor((profile.timeLeft?.seconds-Timestamp.now().seconds)/3600);
+      return hours;
+    }
+    return 0;
    }
 
   customCounterFormatter(inputLength: number, maxLength: number) {
