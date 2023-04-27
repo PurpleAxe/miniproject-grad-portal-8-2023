25823c11d68f572e2ca28922f4f789c65c763e81
VaghelisGkikas <110725930+VaghelisGkikas@users.noreply.github.com>
2023-04-27 21:53:22 +0200

    Update comments.repository.ts

--- libs/api/comments/data-access/src/comments.repository.ts
+++ libs/api/comments/data-access/src/comments.repository.ts
@@ -1,7 +1,23 @@
 import { IComment } from '@mp/api/comments/util';
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
 @Injectable()
 export class CommentsRepository {
   //TODO: add your functions here
+  async search_for(key: string, value: any, collection: string): Promise<[]> {
+    var response = null;
+    const f = await admin
+    .firestore()
+    .collection(collection)
+    .where(key, "==", value)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        response = doc.data().comments;
+      });
+    });
+    return response;
+  }
+  
 }
