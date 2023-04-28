318d89df2c04006a78b83f0bc750d421daf40e5d
JuliannaVenter <u20433752@tuks.co.za>
2023-04-28 17:37:44 +0200

    Bug fixes

--- libs/app/home/feature/src/home.page.ts
+++ libs/app/home/feature/src/home.page.ts
@@ -1,18 +1,16 @@
-import { Component, Renderer2, OnInit } from '@angular/core';
-import { User } from '@angular/fire/auth';
-import { Router } from '@angular/router';
-import { AlertController, MenuController } from '@ionic/angular';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { IProfile } from '@mp/api/profiles/util';
-import { AuthState } from '@mp/app/auth/data-access';
 import { ProfileState } from '@mp/app/profile/data-access';
-import { UserProfileState } from '@mp/app/user-profile/data-access';
-import { SharedModule, SharedPageComponent } from '@mp/app/shared/feature'
-// import { SharedModule } from '@mp/app/shared/feature';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { Logout, SubscribeToAuthState } from '@mp/app/auth/util';
+import { Router } from '@angular/router';
+import { AlertController, MenuController } from '@ionic/angular';
+import { SubscribeToAuthState } from '@mp/app/auth/util';
+import { AuthState } from '@mp/app/auth/data-access';
+import { User } from '@angular/fire/auth';
+import { Logout } from '@mp/app/auth/util';
+import { SharedPageComponent } from '@mp/app/shared/feature';
 import { SubscribeToUserProfile } from '@mp/app/user-profile/util';//'../../util/src/user-profile.actions';
-import { Timestamp } from '@angular/fire/firestore';
 
 @Component({
   selector: 'ms-home-page',
@@ -20,8 +18,6 @@ import { Timestamp } from '@angular/fire/firestore';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  @Select(UserProfileState.userProfile) userProfile$!: Observable<IProfile | null>;
-  shared : any;
   @Select(ProfileState.profile) profile$!: Observable<IProfile | null>;
   @Select(AuthState.user) authProfile$!: Observable<User | null>;
 
@@ -31,12 +27,7 @@ export class HomePage implements OnInit {
   complete : any;
   iconColor: any;
   route : any;
-  hrsTime:any;
-  minutesTime:any;
-  secondsTime:any;
-  hoursString:any;
-  minutesString:any;
-  secondsString:any;
+  shared : any;
 
 
   constructor(
@@ -50,6 +41,14 @@ export class HomePage implements OnInit {
       console.log(profile);
     });
 
+    
+
+    this.shared = new SharedPageComponent(new AlertController, store);
+
+    // this.shared.calculateTimeDifference();
+    // this.shared.decreaseTime('hours', 'minutes', 'ses'); //It is no longer necessary to decrease the time since we have a due date from firestore.
+    // this.shared.decreaseTime('hoursM', 'minutesM', 'sesM');
+    
 
     this.authProfile$.subscribe((var2) => {
       if (var2) {
@@ -81,35 +80,6 @@ export class HomePage implements OnInit {
     });
 
   }
-
-  // getSeconds(profile: IProfile){
-  //   //SharedModule.se
-  //   if (profile.timeLeft?.seconds!=undefined){
-  //     const seconds = (profile.timeLeft?.seconds-Timestamp.now().seconds)%60;
-  //     // console.log(profile.timeLeft?.seconds-Timestamp.now().seconds);
-  //     // if (profile.timeLeft?.seconds-Timestamp.now().seconds<=0){
-  //     //   this.presentAlert();
-  //     // }
-  //     return seconds;
-  //   }
-  //   return 0;
-  //  }
-
-  //  getMinutes(profile: IProfile){
-  //   if (profile.timeLeft?.seconds!=undefined){
-  //     const minutes = Math.floor(((profile.timeLeft?.seconds-Timestamp.now().seconds)%3600)/60);
-  //     return minutes;
-  //   }
-  //   return 0;
-  //  }
-
-  //  getHours(profile: IProfile){
-  //   if (profile.timeLeft?.seconds!=undefined){
-  //     const hours = Math.floor((profile.timeLeft?.seconds-Timestamp.now().seconds)/3600);
-  //     return hours;
-  //   }
-  //   return 0;
-  //  }
 
   async presentAlert() {
     const alert = await this.alertController.create({
