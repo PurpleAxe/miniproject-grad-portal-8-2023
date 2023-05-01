bc89e6d63c5a9de0b645ee5900c25f58dc8df0fb
GD Radloff <danielrad082@gmail.com>
2023-05-01 13:42:41 +0200

    Added new user message saga

--- libs/api/post/feature/src/post.sagas.ts
+++ libs/api/post/feature/src/post.sagas.ts
@@ -1,8 +1,29 @@
+import {UserCreatedEvent} from '@mp/api/users/util';
 import { Injectable } from '@nestjs/common';
-import { ICommand, ofType, Saga } from '@nestjs/cqrs';
+import { EventBus, ICommand, ofType, Saga } from '@nestjs/cqrs';
 import { map, Observable } from 'rxjs';
+import {CreatePostCommand} from '../../util/src/commands';
 
 @Injectable()
 export class PostSagas {
-    
+  constructor(private readonly eventBus: EventBus) {};
+  @Saga()
+  onUserCreated = (events$: Observable<any>): Observable<ICommand> => {
+    return events$.pipe(
+      ofType(UserCreatedEvent),
+      map(
+        (event: UserCreatedEvent) =>
+          new CreatePostCommand({
+            post: {
+              userId: event.user.id,
+              userName: event.user.displayName,
+              challenge: "newMemberChallenge",
+              department: "newbie",
+              message: "Hi everyone, im on timehive",
+              postId:null
+            }
+          })
+      )
+    );
+  };
 }
